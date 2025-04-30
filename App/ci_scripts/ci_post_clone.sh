81d0e212b0ace7f4667b4dfe4f2b3cfc2fd867b5
haludoll <guiltaugusthird03@gmail.com>
2025-04-30 15:10:47 +0900

    Re-create ci_scripts/ci_post_clone.sh

--- App/ci_scripts/ci_post_clone.sh
+++ App/ci_scripts/ci_post_clone.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  talk-assistant
 #
-//  ci_post_clone.sh
-//  talk-assistant
-//
-//  Created by haludoll on 2025/04/27.
-//
+#  Created by haludoll on 2025/04/30.
+#
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
